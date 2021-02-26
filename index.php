1cc45f6d73e65a7bdaeac3073ac6788e491885ff
PaperCore <redev@redevs.ga>
2021-02-26 21:31:44 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,288 @@
+<html lang="ko"><head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" sizes="180.180" href="https://withun.link/identity/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="192x192" href="https://www.withun.link/identity/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://www.withun.link/identity/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://www.withun.link/identity/favicon-16x16.png">
+<link rel="manifest" href="https://withun.link/identity/site.webmanifest">
+<meta name="msapplication-TileColor" content="#1D4ED8">
+<meta name="theme-color" content="#1D4ED8">
+<title>PaperCore | 메시징의 변화를, 삶의 변화를.</title>
+<meta name="title" content="PaperCore | 메시징의 변화를, 삶의 변화를.">
+<meta name="description" content="Unlink는 PaperCore의 패밀리입니다.">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://www.withun.link/">
+<meta property="og:title" content="PaperCore | 메시징의 변화를, 삶의 변화를.">
+<meta property="og:description" content="Unlink는 PaperCore의 패밀리입니다.">
+<meta property="og:image" content="https://www.withun.link/identity/papercore_new.png">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://www.withun.link/">
+<meta property="twitter:title" content="PaperCore | 메시징의 변화를, 삶의 변화를.">
+<meta property="twitter:description" content="Unlink는 PaperCore의 패밀리입니다.">
+<meta property="twitter:image" content="https://www.withun.link/identity/papercore_new.png">
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css" rel="stylesheet">
+
+</head>
+<body>
+<style>
+  .mega-menu {
+    display: none;
+    left: 0;
+    position: absolute;
+    text-align: left;
+    width: 100%;
+  }
+  .hoverable {
+    position: static;
+  }
+  .hoverable > a:after {
+    content: "\25BC";
+    font-size: 10px;
+    padding-left: 6px;
+    position: relative;
+    top: -1px;
+  }
+  .hoverable:hover .mega-menu {
+    display: block;
+  }
+  .toggleable > label:after {
+    content: "\25BC";
+    font-size: 10px;
+    padding-left: 6px;
+    position: relative;
+    top: -1px;
+  }
+  .toggle-input {
+    display: none;
+  }
+  .toggle-input:not(checked) ~ .mega-menu {
+    display: none;
+  }
+  .toggle-input:checked ~ .mega-menu {
+    display: block;
+  }
+  .toggle-input:checked + label {
+    color: white;
+    background: #2c5282;
+  }
+  .toggle-input:checked ~ label:after {
+    content: "\25B2";
+    font-size: 10px;
+    padding-left: 6px;
+    position: relative;
+    top: -1px;
+  }
+.socialicons{
+	height:1rem;
+	}
+  </style>
+<nav class="relative bg-white border-b-2 border-gray-300 text-gray-900">
+<div class="container mx-auto flex justify-between">
+<a href="https://withun.link">
+<div class="flex items-center flex-no-shrink relative block p-4 lg:p-6 text-xl text-blue-600 font-bold">
+<img src="https://withun.link/identity/Group%201.svg" class="h-5" alt="페이퍼코어 로고">PaperCore</div></a>
+<ul class="flex">
+
+<li class="hover:bg-blue-800 hover:text-white flex">
+<a href="https://github.com/papercore-dev" class="flex relative block py-6 px-2 lg:p-6 text-sm lg:text-base font-bold"><img src="https://withun.link/socialicons/gh-mini.png" alt="깃허브 옥토캣" class="socialicons flex">GitHub</a>
+</li>
+
+<li class="hoverable hover:bg-indigo-800 hover:text-white">
+<a href="#" class="relative block py-6 px-4 lg:p-6 text-sm lg:text-base font-bold hover:bg-blue-800 hover:text-white">서비스</a>
+<div class="p-6 mega-menu mb-16 sm:mb-0 shadow-xl bg-indigo-800">
+<div class="container mx-auto w-full flex flex-wrap justify-between mx-2">
+<div class="w-full text-white mb-8">
+<h2 class="font-bold text-2xl">사용자를 위해 사용자 중심적으로 생각한 서비스</h2>
+<p>리크봇부터 모임챗까지. 상상하지 못했던 혁신적인 서비스.</p>
+</div>
+<ul class="px-4 w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-r lg:border-b-0 pb-6 pt-6 lg:pt-3">
+<div class="flex items-center">
+<img class="h-8 mb-3 mr-3 fill-current text-white" src="https://withun.link/identity/lkbt.svg">
+<h3 class="font-bold text-xl text-white text-bold mb-2">리크봇</h3>
+</div>
+<p class="text-gray-100 text-sm">50명이 신뢰한 카카오톡 메신저 봇.</p>
+<div class="flex items-center py-3"> <svg class="h-6 pr-3 fill-current text-blue-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+<path d="M20 10a10 10 0 1 1-20 0 10 10 0 0 1 20 0zm-2 0a8 8 0 1 0-16 0 8 8 0 0 0 16 0zm-8 2H5V8h5V5l5 5-5 5v-3z"></path>
+</svg>
+<a href="https://withun.link/leek" class="text-white bold border-b-2 border-blue-300 hover:text-blue-300">더 알아보기</a>
+</div>
+</ul>
+<ul class="px-4 w-full sm:w-1/2 lg:w-1/4 border-gray-600 border-b sm:border-r-0 lg:border-r lg:border-b-0 pb-6 pt-6 lg:pt-3">
+<div class="flex items-center"> <img class="h-8 mb-3 mr-3 fill-current text-white" src="https://withun.link/identity/dman.svg">
+<h3 class="font-bold text-xl text-white text-bold mb-2">도메인</h3>
+</div>
+<p class="text-gray-100 text-sm">페이퍼코어가 보유한 다양한 도메인으로 나만의 웹페이지를 만들어보세요.</p>
+<div class="flex items-center py-3">
+<svg class="h-6 pr-3 fill-current text-blue-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+<path d="M20 10a10 10 0 1 1-20 0 10 10 0 0 1 20 0zm-2 0a8 8 0 1 0-16 0 8 8 0 0 0 16 0zm-8 2H5V8h5V5l5 5-5 5v-3z"></path>
+</svg>
+<a href="https://withun.link/#" class="text-white bold border-b-2 border-blue-300 hover:text-blue-300">내 도메인 얻기</a>
+</div>
+</ul>
+
+</div>
+</div>
+</li>
+
+</ul>
+</div>
+</nav>
+<div>
+<style>
+
+@layer utilities {
+  .text-gradient {
+    background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+}
+</style>
+<section class="homescreen m-0 flex flex-col w-screen justify-center bg-gray-800 h-80 text-gray-100  ">
+<h1 class="text-5xl  my-auto mx-auto  md:mx-48 ">
+<img src="https://avatars.githubusercontent.com/u/58584157?s=460&amp;u=204ece99ed17a2061b46570ac65677af9a4bb654&amp;v=4" class="rounded-full border-solid border-white border-2 -mt-3 h-16"><strong><span class="text-gradient bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-500 font-mono">PaperCore</span></strong>-dev<br>
+<span class="inline-block rounded-full text-white text-lg tracking-tight  mr-3">반가워요! PaperCore입니다.</span>
+<br>
+<a href="https://commerce.coinbase.com/embed/charges/JQXKFDQT?version=201807&amp;origin=https%3A%2F%2Fwithun.link" target="_blank">
+<button type="button" class="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg">가상화폐로 기부하기</button>
+</a>
+</h1>
+</section>
+<script type="text/javascript">
+/*for (var i = 0; i < 100; i++) {
+  var star =
+    '<div class="star m-0" style="animation: twinkle ' +
+    (Math.random() * 5 + 5) +
+    's linear ' +
+    (Math.random() * 1 + 1) +
+    's infinite; top: ' +
+    Math.random() * $(window).height() +
+    'px; left: ' +
+    Math.random() * $(window).width() +
+    'px;"></div>';
+  $('.homescreen').append(star);
+}*/
+
+</script>
+<div class="px-6 py-8 dark:bg-gray-800 dark:text-gray-600">
+<div class="flex justify-between container mx-auto">
+<div class="w-full lg:w-8/12">
+<div class="flex items-center justify-between">
+<h1 class="text-xl font-bold text-gray-700 md:text-2xl">포스트</h1>
+<div>
+<select class="w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+<option>최신순</option>
+</select>
+</div>
+</div>
+
+<table class="min-w-full table-auto"> <tbody><tr> <th></th> </tr> <tr> <td><div class="mt-6">
+<div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
+<div class="flex justify-between items-center"><span class="font-light text-gray-600"></span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">환영합니다!</a>
+</div>
+<div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline">Welcome!</a>
+<p class="mt-2 text-gray-600">안녕하세요 :) 페이퍼코어 관리자 PaperCore-dev입니다. 페이퍼코어는 2021년 2월 11에 오픈한 따끈따끈한 신생 웹사이트로, 현재 파트너를 모집하고 있습니다! 궁금한 점이 있다면, SUPPORT@WITHUN.LINK로 메일을 주세요.</p>
+</div>
+<div class="flex justify-between items-center mt-4">
+<div><a href="#" class="flex items-center"><img src="https://avatars.githubusercontent.com/u/58584157?s=460&amp;u=204ece99ed17a2061b46570ac65677af9a4bb654&amp;v=4" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
+<h1 class="text-gray-700 font-bold hover:underline">papercore-dev</h1>
+</a></div>
+</div>
+</div>
+</div></td> </tr> <tr> <td><div class="mt-6">
+<div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
+<div class="flex justify-between items-center"><span class="font-light text-gray-600"></span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">페이퍼코어 서비스</a>
+</div>
+<div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline">the Switch가 왜 좋아요?</a>
+<p class="mt-2 text-gray-600">VPN을 무료로 제공하는 것이 가장 큰 이유입니다. 다른 VPN의 경우는 적게는 만원부터, 크게는 한달에 10만원을 호가하는 초 고가 VPN이기 때문에 누구나 쉽게 손 댈수 없습니다. 그러나, 저희 페이퍼코어의 the Switch Warp는 무료인데도 불구하고 no-log 정책과 진짜 VPN을 제공합니다. 광고 차단 VPN과 DNS로서 입지를 넓히고 점점 발전할 The Switch WARP를 응원해 주세요!</p>
+ </div>
+<div class="flex justify-between items-center mt-4"><a href="https://withun.link/index-parentblock.php" class="text-blue-500 hover:underline">지금 Warp!</a>
+<div><a href="#" class="flex items-center"><img src="https://avatars.githubusercontent.com/u/58584157?s=460&amp;u=204ece99ed17a2061b46570ac65677af9a4bb654&amp;v=4" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
+<h1 class="text-gray-700 font-bold hover:underline">papercore-dev</h1>
+</a></div>
+</div>
+</div>
+</div></td> </tr> <tr> <td><div class="mt-6">
+<div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
+<div class="flex justify-between items-center"><span class="font-light text-gray-600"></span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">페이퍼코어 서비스</a>
+</div>
+<div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline">그래서 리크봇이 뭔데?</a>
+<p class="mt-2 text-gray-600">방장봇 짭.... 아니, 카카오톡과 같은 메신저에서 사용할 수 있는 봇입니다. 24시간 자동으로 답해주고, 말도 잘 듣는 인공지능 메신저 친구입니다. 이루다 사건으로 인해 챗봇에 대한 걱정이 커지겠지만, 리크봇은 성착취로부터 안전하도록 운영자가 수동적으로 인증하고 검토하는 청정한 AI입니다. AI만 장점이다 하고 내세우는것 같지만, 사실 그 외에도 기능이 더 있습니다. 주식게임, 끝말잇기, 검색, 유튜브 구독자 확인... 모두 쉽게쉽게 가능한 채팅방의 즐거움을 더해주는 봇입니다. 카카오톡 오픈채팅방의 대화율이 너무 적고 현재 사용하고 있는 봇이 부족하다 생각한다면, 리크봇으로 팬다봇과 방장봇의 한계를 넘고, 대화량도 늘려 보세요!</p>
+</div>
+<div class="flex justify-between items-center mt-4"><a href="https://withun.link/leek" class="text-blue-500 hover:underline">리크랑 대화 시작!</a>
+<div><a href="#" class="flex items-center"><img src="https://avatars.githubusercontent.com/u/58584157?s=460&amp;u=204ece99ed17a2061b46570ac65677af9a4bb654&amp;v=4" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
+<h1 class="text-gray-700 font-bold hover:underline">papercore-dev</h1>
+</a></div>
+</div>
+</div>
+</div></td> </tr> <tr> <td><div class="mt-6">
+<div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
+<div class="flex justify-between items-center"><span class="font-light text-gray-600"></span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">페이퍼코어 서비스</a>
+</div>
+<div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline" style="font-family:돋움;">맞춤 홈페이지 나 깃 허브로 웹 페이지 만들고 싶나요 ?? 그러니 Gmail (webweju@withun.link) 또는 아래 설명에 저에게 연락하십시</a>
+<p class="mt-2 text-gray-600">여러분, 외주 홈페이지 나 깃 허브로 웹 서버나 웹 페이지 만들고 싶나요 ?? 그러니 Gmail 또는 아래 링크에 저에게 연락하십시오<small>Team Azimkiya 패러디고 번역체 안씁니다.</small></p>
+</div>
+<div class="flex justify-between items-center mt-4"><a href="mailto:webweju@withun.link" class="text-blue-500 hover:underline">연락할</a>
+ <div><a href="#" class="flex items-center"><img src="https://avatars.githubusercontent.com/u/58584157?s=460&amp;u=204ece99ed17a2061b46570ac65677af9a4bb654&amp;v=4" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
+<h1 class="text-gray-700 font-bold hover:underline">papercore-dev</h1>
+</a></div>
+</div>
+</div>
+</div></td> </tr> <tr> <td><div class="mt-6">
+<div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
+<div class="flex justify-between items-center"><span class="font-light text-gray-600"></span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">새해복</a>
+</div>
+<div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline">설</a>
+<p class="mt-2 text-gray-600">새해 복 많이 받으십시오.</p>
+</div>
+<div class="flex justify-between items-center mt-4"><a href="#" class="text-blue-500 hover:underline">새해 복</a>
+<div><a href="#" class="flex items-center"><img src="https://avatars.githubusercontent.com/u/58584157?s=460&amp;u=204ece99ed17a2061b46570ac65677af9a4bb654&amp;v=4" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
+<h1 class="text-gray-700 font-bold hover:underline">papercore-dev</h1>
+</a></div>
+</div>
+</div>
+</div></td> </tr></tbody></table> 
+</div>
+<div class="-mx-8 w-4/12 hidden lg:block">
+</div>
+</div>
+</div>
+</div>
+<footer class="bg-gray-800 pt-10 sm:mt-10 pt-10">
+<div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
+<div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+<div class="text-xs uppercase text-gray-400 font-medium mb-6">페이퍼코어 패밀리</div>
+<a href="https://withun.link/leek" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">리크봇</a>
+<a href="https://withun.link/dev" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">PaperCore Developers</a>
+<a href="https://withun.link/ad" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">광고</a>
+</div>
+<div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+<div class="text-xs uppercase text-gray-400 font-medium mb-6">개인정보 센터</div>
+<a href="https://withun.link/privacy/" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보 센터</a>
+<a href="https://withun.link/privacy/tos.html" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">이용 약관</a>
+<a href="https://withun.link/privacy/eula.html" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보처리방침</a>
+<a href="https://withun.link/privacy/disclaimer.html" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">면책 조항</a>
+</div>
+<div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+<div class="text-xs uppercase text-gray-400 font-medium mb-6">웹 서비스</div>
+<a href="https://withun.link/index-parentblock.php" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch WARP</a>
+<a href="https://withun.link/index-parentblock.php" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch DNS</a>
+</div>
+</div>
+<div class="pt-2">
+<div class="flex pb-5 px-3 m-auto pt-5 
+            border-t border-gray-500 text-gray-400 text-sm 
+            flex-col md:flex-row max-w-6xl">
+<div class="mt-2">
+© 페이퍼코어 2018~2021 모든 권리 보유. <br><small>페이퍼코어로 발생한 상거래의 책임은 구매자에게 있습니다. 페이퍼코어는 사업자가 아닙니다.</small></div>
+<div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+</div>
+</div>
+</div>
+</footer>
+<script async="" defer="" src="https://scripts.simpleanalyticscdn.com/latest.js" type="text/javascript"></script>
+<noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt=""/></noscript>
+
+
+</body></html>
